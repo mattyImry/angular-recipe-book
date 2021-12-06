762db921975a751cc68d1101a7f6190f963993ff
mattyImry <matteo.imeri@gmail.com>
2021-12-06 16:53:17 +0000

    Add local reference

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -16,9 +16,9 @@ export class CockpitComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddServer() {
+  onAddServer(serverNameInput: HTMLInputElement) { //server input is a local reference in cockpit.html
     this.serverCreated.emit({
-      serverName: this.newServerName,
+      serverName: serverNameInput.value,
       serverContent: this.newServerContent
     });
   }
