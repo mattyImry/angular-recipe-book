5401b90cd86673f5a8deb8ba655e96bbeb227fcf
mattyImry <matteo.imeri@gmail.com>
2021-12-09 16:32:21 +0000

    Tested hookes with related methods

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -4,7 +4,13 @@ import {
   Input, 
   ViewEncapsulation, 
   OnChanges, 
-  SimpleChanges
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
 } from '@angular/core';
 
 @Component({
@@ -13,14 +19,22 @@ import {
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated   // u can choose to none or shadow dome. Emulated is default 
 })
-export class ServerElementComponent implements OnInit, OnChanges {
+export class ServerElementComponent implements 
+OnInit, 
+OnChanges, 
+DoCheck, 
+AfterContentInit,
+AfterContentChecked,
+AfterViewInit,
+AfterViewChecked,
+OnDestroy{
   @Input('srvElement') element: {type: string, name: string, content: string}; // by using `@input` and update @angular/core with `Input` you can use this property enywhere you can add an alias in the () to be able to use a different name outside you r component but keeping property name in your component
   @Input() name: string
   constructor() { 
     console.log('constructor called!');
 
   }
-
+  
   ngOnChanges(changes: SimpleChanges) {
     console.log('ngOnChanges called!');
     console.log(changes);
@@ -30,4 +44,27 @@ export class ServerElementComponent implements OnInit, OnChanges {
     console.log('ngOnInit called!');
   }
 
+  ngDoCheck() {
+  console.log('ngDocheck called!');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentinit called!');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called!');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy() {
+    console.log('OnDestroy called!');
+  }
 }
