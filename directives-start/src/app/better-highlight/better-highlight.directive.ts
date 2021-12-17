5e42ffeb50b0e388e33c65c5d866c79207f09dd5
mattyImry <matteo.imeri@gmail.com>
2021-12-17 16:13:24 +0000

    Add hostbinding

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,9 +1,19 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { 
+  Directive, 
+  ElementRef, 
+  HostBinding, 
+  HostListener, 
+  OnInit, 
+  Renderer2 
+} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+
+  // hostbinding lets you style directly with a property we also give a value of transparent to the property backgroundColor: string to avoid errors
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
 
   // add renderer (is a variable):Renderer2 to use setStyle() need to add again private namevar: ElementRef to refer to the element in html
   constructor( private elRef: ElementRef, private renderer: Renderer2) { }
@@ -17,10 +27,12 @@ export class BetterHighlightDirective implements OnInit {
   // add @HostListener which takes an argument as one of the vents supported in the dom
   //you can also pass in the method the type of data in this case event or you can also listen to custom events 
   @HostListener('mouseenter') mouseOver(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'blue';
   } 
 
   @HostListener('mouseleave') mouseLeave() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 }                                                                                 
