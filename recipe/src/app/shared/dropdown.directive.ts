dca374cc6844113f940141c9cb04aaccc1f3889b
mattyImry <matteo.imeri@gmail.com>
2021-12-21 16:19:40 +0000

    Add ability to close dropdown anywhere in the document

--- recipe/src/app/shared/dropdown.directive.ts
+++ recipe/src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, HostListener } from "@angular/core";
+import { Directive, HostBinding, HostListener,ElementRef} from "@angular/core";
 
 @Directive({
     selector: '[appDropdown]'
@@ -6,7 +6,13 @@ import { Directive, HostBinding, HostListener } from "@angular/core";
 export class DropdownDirective {
     @HostBinding('class.open') isOpen = false; //binding property by attaching the opne css class to elemeent
 
-    @HostListener('click') toggleOpen() { //listening to click event on element where the directive in put
-        this.isOpen = !this.isOpen;
-    }
+    //@HostListener('click') toggleOpen() { //listening to click event on element where the directive in put
+        //this.isOpen = !this.isOpen;
+    
+
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false; //closing the drop down anywehre placing the listener in the document
+      }
+
+    constructor(private elRef: ElementRef) {}
 }
