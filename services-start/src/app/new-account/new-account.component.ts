95b860ac2fbedfc53b18fdf4d6f203d10916b8e0
mattyImry <matteo.imeri@gmail.com>
2021-12-27 18:00:22 +0000

    Add account service

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component} from '@angular/core';
+import { AccountService } from '../account.service';
 import { LoggingService } from '../logging.service';
 
 @Component({
@@ -8,16 +9,12 @@ import { LoggingService } from '../logging.service';
   providers: [LoggingService] // provide the service
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor(private loggingService: LoggingService ) {} //crate a constructor to be able to use the LoggingService functionality
+  constructor(private loggingService: LoggingService,
+              private accountService: AccountService ) {} //crate a constructor to be able to use the LoggingService functionality
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    
+    this.accountService.addAccount(accountName, accountStatus);
     this.loggingService.logStatusChange(accountStatus); // accessing  the logginService property to create the instance automatically
   }
 }
