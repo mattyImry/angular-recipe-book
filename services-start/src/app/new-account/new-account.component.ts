d217b7d0d9658dc25c81f50c6095cb101aa0e23b
mattyImry <matteo.imeri@gmail.com>
2021-12-30 12:20:22 +0000

    Add event emitter to service

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -10,8 +10,15 @@ import { LoggingService } from '../logging.service';
 })
 export class NewAccountComponent {
 
+  //crate a constructor to be able to use the LoggingService functionality
   constructor(private loggingService: LoggingService,
-              private accountService: AccountService ) {} //crate a constructor to be able to use the LoggingService functionality
+              private accountService: AccountService ) { 
+    
+    // from account service the emitter is wrapped in a subscribe so i get get ist ans give an alert for any status change
+     this.accountService.statusUpdated.subscribe(
+       (status:string) => alert('New status: ' + status)
+     )
+  } 
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
