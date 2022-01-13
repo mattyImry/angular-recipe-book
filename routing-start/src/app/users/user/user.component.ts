34d865a700c2fec1a986046f1ee549102cf4f93e
mattyImry <matteo.imeri@gmail.com>
2022-01-13 17:55:05 +0000

    Fetching Route Parameters Reactively

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -16,7 +16,14 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'], // this is need to get the dynamic route/url for user id and name from line 21 app module
       name: this.route.snapshot.params['name']
     };
+
+    //to access the user when already inside the component otherwise angular won't waste performance to reload if already in the component so not update on data
+    this.route.params
+      .subscribe(
+          (params: Params) => {
+            this.user.id = params['id'];
+            this.user.name = params['name'];
+          }
+      );
   }
-
-
 }
