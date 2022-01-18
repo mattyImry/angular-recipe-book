d81fbd48ddefde70e9c14e04ef5024a25656af4c
mattyImry <matteo.imeri@gmail.com>
2022-01-18 17:54:04 +0000

    Add not found component and route

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 //create an array Routes to hold the routing. Path = name of routing displayed in url ,component = which component
@@ -23,7 +24,9 @@ const appRoutes: Routes = [
   { path: 'servers', component: ServersComponent, children: [ // adding chidlren: [] you nest all url using servers 
     { path: ':id', component: ServerComponent },
     { path: ':id/edit', component: EditServerComponent }
-  ] }
+  ] },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found' } //by using the ** wild card for route will catch all the url not known in the app has to be the last one in the list of routes
 ];
 
 @NgModule({
@@ -34,7 +37,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
