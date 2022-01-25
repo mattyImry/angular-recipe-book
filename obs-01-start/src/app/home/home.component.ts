58ff9f4a49d7bc407acf1591c3314749f3b33110
mattyImry <matteo.imeri@gmail.com>
2022-01-25 12:45:55 +0000

    Created custom observable

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObsSubscription: Subscription; // 2 add a Subscription
 
   constructor() { }
 
   ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe(count => { // 1 observable keeps emitting value. To stop it you have to unsubscribe
+    //   console.log(count)
+    // });
+    const customIntervalObserver = new Observable(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObserver.subscribe(data => {
+      console.log(data);
+      
+    })
+  }
+
+  ngOnDestroy(): void { //3 onDestroy to unsubscribe to the sunscription every time i leave that component i unsubscribe to save resources
+      this.firstObsSubscription.unsubscribe();
   }
 
 }
