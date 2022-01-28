c79a9bd5d6efb50a1793620f1dfd4d21dca2bc23
mattyImry <matteo.imeri@gmail.com>
2022-01-28 16:06:17 +0000

    Remove event emitter add subjects

--- recipe/src/app/app.component.ts
+++ recipe/src/app/app.component.ts
@@ -6,10 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadedFeature = 'recipe';  // setting the property loadedFeature the same 'recipe' line 11 header.html
-
-
-  onNavigate(feature: string) { 
-    this.loadedFeature = feature; 
+ 
   }
-}
+
--- recipe/src/app/recipes/recipe.service.ts
+++ recipe/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
+
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
@@ -9,8 +10,7 @@ import { Recipe } from './recipe.model';
   providedIn: 'root'
 })
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
-
+  
   private recipes: Recipe[] = [
     new Recipe(
       'Spetzel',
