2f25c42d42f970f6716471cfefd28a87e3fba646
mattyImry <matteo.imeri@gmail.com>
2022-01-31 12:16:41 +0000

    Access object form with @ViewChild

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,12 +7,19 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm; //getting asses to the form using view @child
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
-  onSubmit(form: NgForm) {
-    console.log(form);
+  // onSubmit(form: NgForm) { //get asses to the form using (ngSubmit)="onSubmit(f)" #f="ngForm" in the form tag
+  //   console.log(form);
+    
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
     
   }
 }
